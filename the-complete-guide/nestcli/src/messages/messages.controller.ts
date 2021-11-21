ba109b6d444c3c7d7844a23a8a7bb7f37dda2213
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-11-21 09:58:06 +0630

    add: validation rules to particular handler

--- the-complete-guide/nestcli/src/messages/messages.controller.ts
+++ the-complete-guide/nestcli/src/messages/messages.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Post, Body, Param } from "@nestjs/common";
-
+import { CreateMessageDto } from "./dtos/createMessage.dto";
 @Controller("messages")
 export class MessagesController {
     @Get("/")
     getAllMessages() {}
 
     @Post("/")
-    craeteNewMessage(@Body() body: any) {
+    craeteNewMessage(@Body() body: CreateMessageDto) {
         console.log(body);
     }
 
