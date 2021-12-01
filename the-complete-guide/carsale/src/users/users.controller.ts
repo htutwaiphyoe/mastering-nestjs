b67eca97ff15060dcbf155fbbf9622e86f87d553
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-12-01 14:39:26 +0630

    add: api for create new user

--- the-complete-guide/carsale/src/users/users.controller.ts
+++ the-complete-guide/carsale/src/users/users.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Post, Body } from "@nestjs/common";
 
+import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dtos/createUser.dto";
+
 @Controller("auth")
 export class UsersController {
+    constructor(private userService: UsersService) {}
+
     @Post("/signup")
-    createUser(@Body() body: CreateUserDto) {
-        return body;
+    async createUser(@Body() body: CreateUserDto) {
+        const createdUser = await this.userService.create(body.email, body.password);
+        return {
+            status: "success",
+            data: createdUser,
+        };
     }
 }
