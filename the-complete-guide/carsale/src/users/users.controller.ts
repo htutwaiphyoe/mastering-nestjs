74e707414fb31d5cc2a804ef096c23868313c806
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-12-04 01:11:15 +0630

    add: get api for single and multiple users

--- the-complete-guide/carsale/src/users/users.controller.ts
+++ the-complete-guide/carsale/src/users/users.controller.ts
@@ -1,21 +1,37 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Query, Body, Get, Param, NotFoundException } from "@nestjs/common";
 
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dtos/createUser.dto";
 
 @Controller("auth")
 export class UsersController {
-   constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService) {}
 
-   @Post("/signup")
-   async createUser(@Body() body: CreateUserDto) {
-      const createdUser = await this.userService.create(
-         body.email,
-         body.password
-      );
-      return {
-         status: "success",
-         data: createdUser,
-      };
-   }
+  @Post("/signup")
+  async createUser(@Body() body: CreateUserDto) {
+    const createdUser = await this.userService.create(body.email, body.password);
+    return {
+      status: "success",
+      data: createdUser,
+    };
+  }
+
+  @Get("/:id")
+  async findUser(@Param("id") id: string) {
+    const user = await this.userService.findOne(+id);
+    if (!user) throw new NotFoundException("No user found!!!");
+    return {
+      status: "success",
+      data: user,
+    };
+  }
+
+  @Get("/")
+  async find(@Query("email") email: string) {
+    const user = await this.userService.find(email);
+    return {
+      status: "success",
+      data: user,
+    };
+  }
 }
