e75626d455d094bc4aee536a0218f9c63be0db13
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-12-04 00:37:14 +0630

    add: remove user

--- the-complete-guide/carsale/src/users/users.service.ts
+++ the-complete-guide/carsale/src/users/users.service.ts
@@ -6,31 +6,35 @@ import { User } from "./user.entity";
 
 @Injectable()
 export class UsersService {
-   constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
 
-   create(email: string, password: string) {
-      const user = this.userRepo.create({
-         email,
-         password,
-      });
+  create(email: string, password: string) {
+    const user = this.userRepo.create({
+      email,
+      password,
+    });
 
-      return this.userRepo.save({ email, password });
-   }
+    return this.userRepo.save({ email, password });
+  }
 
-   findOne(id: number) {
-      return this.userRepo.findOne(id);
-   }
+  findOne(id: number) {
+    return this.userRepo.findOne(id);
+  }
 
-   find(email: string) {
-      return this.userRepo.find({ email });
-   }
+  find(email: string) {
+    return this.userRepo.find({ email });
+  }
 
-   async update(id: number, body: Partial<User>) {
-      let user = await this.findOne(id);
-      if (!user) throw new Error("No user found!");
-      Object.assign(user, body);
-      return this.userRepo.save(user);
-   }
+  async update(id: number, body: Partial<User>) {
+    let user = await this.findOne(id);
+    if (!user) throw new Error("No user found!");
+    Object.assign(user, body);
+    return this.userRepo.save(user);
+  }
 
-   remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error("No user found!");
+    return this.userRepo.remove(user);
+  }
 }
