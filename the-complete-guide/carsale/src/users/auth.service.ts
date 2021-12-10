b170262604c027e27a2987ca942f9235d2d7fed9
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-12-10 08:59:17 +0630

    add: sign up checking email exists

--- the-complete-guide/carsale/src/users/auth.service.ts
+++ the-complete-guide/carsale/src/users/auth.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 
 import { UsersService } from "./users.service";
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async signUp(email: string, password: string) {
+    // check email exists
+    const users = await this.userService.find(email);
+    if (users.length) throw new BadRequestException("Email is already in use.");
+    // hash the password
+    // return new user
+  }
+
+  signIn() {}
 }
